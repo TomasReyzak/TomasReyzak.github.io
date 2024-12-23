02b5e0a355dfa8d15b0490ac30f20ba34b0350d7
TomasReyzak <zaktimur2002@gmail.com>
2024-12-23 15:32:45 +0300

    Update script.js

--- Second2/scripts/script.js
+++ Second2/scripts/script.js
@@ -1,2 +1,12 @@
-alert('Привет, мир!');
-alert(2+2+1)
+"use strict"
+let user = 'Tom';
+let message = 'Hello';
+message = user;
+alert(message);
+
+ let $ = 1;
+ let _ = 2;
+ alert($ + _);
+
+ const myBirthday = '01.01.1990';
+ myBirthday = '02.02.2000'; // ошибка, константу нельзя перезаписать!
