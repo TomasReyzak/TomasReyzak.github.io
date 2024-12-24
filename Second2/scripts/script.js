587358e65304ba3699f12b44ddd703184b57792a
TomasReyzak <zaktimur2002@gmail.com>
2024-12-24 14:55:38 +0300

    Update script.js

--- Second2/scripts/script.js
+++ Second2/scripts/script.js
@@ -4,17 +4,21 @@ let message = 'Hello';
 message = user;
 alert(message);
 
- let $ = 1;
- let _ = 2;
- alert($ + _);
+alert(`Привет, ${user}!`);
 
- const COLOR_RED = "#F00";
- const COLOR_GREEN = "#0F0";
- const COLOR_BLUE = "#00F";
- const COLOR_ORANGE = "#FF7F00";
+let $ = 1;
+let _ = 2;
+alert($ + _);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
  
- let color = COLOR_ORANGE;
- alert(color); // #FF7F00
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+
 
 /* const myBirthday = '01.01.1990';
    myBirthday = '02.02.2000'; // ошибка, константу нельзя перезаписать! */
