0f7e913a356347dcade246d9ff60f0deb65c3bba
TomasReyzak <zaktimur2002@gmail.com>
2024-12-26 11:32:05 +0300

    Update script.js

--- Second2/scripts/script.js
+++ Second2/scripts/script.js
@@ -1,24 +1,5 @@
-
-let user = 'Tom';
-let message = 'Hello';
-message = user;
-alert(message);
-
+let user = prompt('Как тебя зовут?','Тони');
 alert(`Привет, ${user}!`);
 
-let $ = 1;
-let _ = 2;
-alert($ + _);
-
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
- 
-let color = COLOR_ORANGE;
-alert(color); // #FF7F00
-
-
-
-/* const myBirthday = '01.01.1990';
-   myBirthday = '02.02.2000'; // ошибка, константу нельзя перезаписать! */
+let isBoss = confirm("Ты здесь главный?")
+alert(isBoss);
